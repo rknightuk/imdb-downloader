e659ecc59b8c3617c6e2662d3c81a205da9cf70c
Robb Lewis <me@therobb.com>
2014-03-19 17:13:22 +0000

    Initial commit

---
+++ background.js
@@ -0,0 +1,11 @@
+// // this is the background code...
+
+// // listen for our browerAction to be clicked
+// chrome.browserAction.onClicked.addListener(function (tab) {
+// 	// for the current tab, inject the "inject.js" file & execute it
+// 	chrome.tabs.executeScript(tab.ib, {
+// 		file: 'inject.js'
+// 	});
+// });
+
+console.log("Loaded!");
---
+++ readme.md
@@ -0,0 +1,3 @@
+# IMDB Download
+
+A chrome extension that adds a download button to IMDB movie pages.
