1c3291d8974826cd8c9e1567378008346ed789f8
Robb Lewis <me@therobb.com>
2014-04-06 15:13:24 +0100

    Fix bug related to watchlist button

--- js/imdb.js
+++ js/imdb.js
@@ -1,7 +1,7 @@
 var site = 'imdb',
 	pageTitle = document.title,
-	buttons = document.getElementById('maindetails_center_bottom');
-buttons.innerHTML = '<div id="download-wrapper"><p>Fetching download links...</p></div><br>' + buttons.innerHTML;
+	buttons = document.getElementById('maindetails_center_top');
+buttons.innerHTML += '<div id="download-wrapper"><p>Fetching download links...</p></div><br>';
 buttons = document.getElementById('download-wrapper');
 if (pageTitle.indexOf("TV Series") != -1) {
 	showMessage("Movie Downloader doesn't currently support TV shows");
