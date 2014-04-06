0b6d5f3823cee617f18d2257c90e4c5afc947d3b
Robb Lewis <me@therobb.com>
2014-04-06 17:20:15 +0100

    Merged scripts into one file

--- js/background.js
+++ js/background.js
@@ -1,3 +1,25 @@
+var pageTitle = document.title,
+	buttons = document.getElementById('maindetails_center_top'),
+	downloadDiv = document.createElement('div');
+
+downloadDiv.id = 'download-wrapper';
+downloadDiv.innerHTML = '<p>Fetching download links...</p>';
+buttons.appendChild(downloadDiv);
+buttons = document.getElementById('download-wrapper');
+
+if (pageTitle.indexOf("TV Series") != -1) {
+	showMessage("Movie Downloader doesn't currently support TV shows");
+}
+else {
+	getPrefs();
+}
+
+function getMovieIdOrTitle() {
+	var keyword = window.location.pathname.split('/');
+	keyword = keyword[2];
+	return keyword;
+}
+
 function getPrefs() {
 	// Get user settings
 	var filePref;
