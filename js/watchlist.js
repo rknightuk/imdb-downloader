ae46553028ee1e328d55c85d8fe662deacce4fa4
Robb Lewis <me@therobb.com>
2014-04-07 19:43:59 +0100

    Fixed progress messages

--- js/watchlist.js
+++ js/watchlist.js
@@ -1,16 +1,15 @@
-console.log("HIIIII");
-
 var filePref,
 	url,
 	movieCount = 0,
+	idCount = 0,
+	checkCount = 0,
 	pageTitle = document.title,
 	buttons = document.getElementById('main'),
 	downloadDiv = document.createElement('div');
 	
 downloadDiv.id = 'download-wrapper';
 downloadDiv.innerHTML = '<p>Fetching download links...</p>';
-$(downloadDiv).insertBefore('#main .list_titles');
-// buttons.appendChild(downloadDiv);
+$(downloadDiv).insertBefore('#main .clear');
 buttons = document.getElementById('download-wrapper');
 
 getPrefs();
@@ -44,7 +43,7 @@ function getWatchlistIds() {
 	        }
 	    }
 	}
- 
+	idCount = links.length;
 	for (i = 0; i < links.length; i++) {
 		id = links[i];
 		console.log(id);
@@ -66,7 +65,7 @@ function getMovies(keyword) {
 		{
 			var movies = JSON.parse(xmlhttp.responseText);
 			movies = movies.MovieList;
-
+			checkCount++;
 			if (movies !== undefined) {
 				movieCount++;
 				if (movieCount === 1) {
@@ -94,7 +93,7 @@ function getMovies(keyword) {
 			}
 			else {
 				console.log(movieCount);
-				if (movieCount === 0) {
+				if (checkCount == idCount && movieCount === 0) {
 					showMessage('No downloads available');
 				}
 			}
