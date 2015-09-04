7b2fae08b84ecb9d779579a74f94720adddf28d3
Robb Lewis <me@therobb.com>
2015-09-04 09:38:04 +0100

    Remove options no longer available in v2 api

--- js/prefs.js
+++
@@ -1,9 +0,0 @@
-var prefs = {};
-
-chrome.storage.sync.get({
-	filePref: 'TorrentMagnetUrl',
-	proxy: true
-}, function(items) {
-	prefs.filePref = items.filePref;
-	prefs.proxy = items.proxy ? 'https://yts.al/api/list.json' : 'http://yts.re/api/list.json';
-});
--- options/options.js
+++ options/options.js
@@ -1,38 +1,3 @@
-function save_options() {
-	var filePref;
-	var filePrefs = document.getElementsByName('download');
-		for (var i = 0; i < filePrefs.length; i++) {
-			if (filePrefs[i].checked) {
-				filePref = filePrefs[i].value;
-				break;
-			}
-	}
-
-	chrome.storage.sync.set({
-		filePref: filePref,
-		proxy: document.getElementById('proxy').checked
-	}, function() {
-		var status = document.getElementById('status');
-		status.textContent = 'Options saved.';
-		setTimeout(function() {
-			status.textContent = '';
-		}, 1500);
-	});
-}
-
-function restore_options() {
-	chrome.storage.sync.get({
-		filePref: 'TorrentMagnetUrl',
-		proxy: true
-	}, function(items) {
-		document.getElementById(items.filePref).checked = true;
-		document.getElementById('proxy').checked = items.proxy;
-	});
-}
-
-document.addEventListener('DOMContentLoaded', restore_options);
-document.getElementById('save').addEventListener('click', save_options);
-
 var manifest = chrome.runtime.getManifest(),
 	versionEl = document.getElementById('version-info');
 
