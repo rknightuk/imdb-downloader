afffee7d8329aeffd8c336519d49ebbd0b5a29ba
Robb Lewis <me@therobb.com>
2015-09-04 10:19:14 +0100

    Update options page

--- options/options.js
+++ options/options.js
@@ -1,4 +1,6 @@
 var manifest = chrome.runtime.getManifest(),
+	descriptionEl = document.getElementById('description'),
 	versionEl = document.getElementById('version-info');
 
+descriptionEl.innerHTML = manifest.description;
 versionEl.innerHTML = 'version ' + manifest.version + versionEl.innerHTML;
